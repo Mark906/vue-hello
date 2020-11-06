adf9d4be7b86b36d47e8abf39d4b9958f9e46485
Marco <markoka1@outlook.it>
2020-11-06 17:29:45 +0100

    Miglioramento codice

--- main.js
+++ main.js
@@ -4,5 +4,25 @@ var app = new Vue({
         messaggio: 'Il primo esercizio con Vue.',
         immagine: "https://technoblitz.it/wp-content/uploads/2018/06/immagini-4k.jpg",
     },
-    methods: { }
+    methods: {
+
+        cambiaImmagine() {
+
+            if (this.immagine == "https://technoblitz.it/wp-content/uploads/2018/06/immagini-4k.jpg")
+            {
+                this.immagine = "https://www.addlance.com/blog/wp-content/uploads/2019/04/immagini-da-scaricare.jpg"
+            }
+
+            else if (this.immagine == "https://www.addlance.com/blog/wp-content/uploads/2019/04/immagini-da-scaricare.jpg")
+            {
+                this.immagine = "https://www.arthemisia.it/wp-content/uploads/2016/04/vittoriano-ligabue-500x500.jpg"
+            }
+
+            else {
+                this.immagine = "https://technoblitz.it/wp-content/uploads/2018/06/immagini-4k.jpg"
+            }
+
+        }
+
+    }
 });
