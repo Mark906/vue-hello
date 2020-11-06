16a6819868dd55c9419f2c7f076d4904bf016d67
Marco <markoka1@outlook.it>
2020-11-06 17:38:16 +0100

    Versione 2

--- main.js
+++ main.js
@@ -3,23 +3,26 @@ var app = new Vue({
     data: { // contenitore di tutte le informazioni necessarie
         messaggio: 'Il primo esercizio con Vue.',
         immagine: "https://technoblitz.it/wp-content/uploads/2018/06/immagini-4k.jpg",
+        mongolfiera: "https://technoblitz.it/wp-content/uploads/2018/06/immagini-4k.jpg",
+        faro: "https://www.addlance.com/blog/wp-content/uploads/2019/04/immagini-da-scaricare.jpg",
+        tigre: "https://www.arthemisia.it/wp-content/uploads/2016/04/vittoriano-ligabue-500x500.jpg"
     },
     methods: {
 
         cambiaImmagine() {
 
-            if (this.immagine == "https://technoblitz.it/wp-content/uploads/2018/06/immagini-4k.jpg")
+            if (this.immagine == this.mongolfiera)
             {
-                this.immagine = "https://www.addlance.com/blog/wp-content/uploads/2019/04/immagini-da-scaricare.jpg"
+                this.immagine = this.faro
             }
 
-            else if (this.immagine == "https://www.addlance.com/blog/wp-content/uploads/2019/04/immagini-da-scaricare.jpg")
+            else if (this.immagine == this.faro)
             {
-                this.immagine = "https://www.arthemisia.it/wp-content/uploads/2016/04/vittoriano-ligabue-500x500.jpg"
+                this.immagine = this.tigre
             }
 
             else {
-                this.immagine = "https://technoblitz.it/wp-content/uploads/2018/06/immagini-4k.jpg"
+                this.immagine = this.mongolfiera
             }
 
         }
