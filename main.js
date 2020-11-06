025644d310630988b0eb60f4945040af40d99dea
Marco <markoka1@outlook.it>
2020-11-06 16:44:05 +0100

    Vue - hello

---
+++ main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#root', // div "radice" di vue
+    data: { // contenitore di tutte le informazioni necessarie
+        messaggio: 'Il primo esercizio con Vue.',
+        immagine: "https://technoblitz.it/wp-content/uploads/2018/06/immagini-4k.jpg",
+    },
+    methods: { }
+});
